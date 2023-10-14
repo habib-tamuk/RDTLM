7af52eceb198f4e3e26859fb5addbf46420b1fb3
Md Habibur Rahman <89242237+habib-tamuk@users.noreply.github.com>
2023-10-14 16:20:14 -0500

    Update VGG16.m

--- VGG16.m
+++ VGG16.m
@@ -114,11 +114,3 @@ avgError = (c5Error+h5Error+p5Error)/3;
 totalImage = sum(confMat, 'all');
 totalTestImage = countcats(YTest);
 totalTrainImage = countcats(YTrain);
-%%
-%Individual Image Test Purpose
-testImage = imread('C:\Users\Lenovo\Downloads\Covid-19\Matlab\Image\COVID\COVID2.png');
-%img = imresize(testImage, [224 224]);
-img = readAndPreprocessImage(testImage); 
-imageFeatures = activations(net, img, layer,'OutputAs','rows','ExecutionEnvironment','cpu');
-%label = predict(classifier.Trained{1,1}, imageFeatures); %for ensemple classifier
-label = predict(classifier, imageFeatures);
